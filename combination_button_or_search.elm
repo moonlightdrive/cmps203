e7a5b7f762f474e05e69ddab77cfe144ab906b51
Husain Kassamnath <hkassamn@ucsc.edu>
2016-03-10 19:26:32 -0800

    Added files via upload

---
+++ combination_button_or_search.elm
@@ -0,0 +1,33 @@
+﻿import Html exposing (div, button, text)
+import Html.Events exposing (onClick)
+import Graphics.Input.Field exposing (..)
+import Graphics.Element exposing (Element)
+import StartApp.Simple as StartApp
+
+
+movie : Signal.Mailbox Content
+movie = Signal.mailbox noContent
+
+movieField : Signal Element
+movieField =
+  Signal.map (field defaultStyle (Signal.message movie.address) "Movie") movie.signal
+
+main : Signal Element
+main = movieField
+
+--main =
+--  StartApp.start { model = model, view = view, update = update }
+
+model = 0
+
+
+view address model =
+  div []
+    [ button [ onClick address Search ] [ text "Search" ]
+
+  ]
+
+type Action = Search
+
+
+update action model = 0
