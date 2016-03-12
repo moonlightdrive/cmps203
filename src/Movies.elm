9c6001dc72523bda3b4c10c86019323dda27f7cb
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2016-03-12 09:53:15 -0800

    remove redundant code from movies

--- src/Movies.elm
+++ src/Movies.elm
@@ -91,14 +91,3 @@ model =
 main : Signal Html
 main =
   Signal.map (view movie.address) model
-movie : Signal.Mailbox Content
-movie = Signal.mailbox noContent
-
-
-movieField : Signal Element
-movieField =
-  Signal.map (field defaultStyle (Signal.message movie.address) "Movie") movie.signal
-
-
-main : Signal Element
-main = movieField
