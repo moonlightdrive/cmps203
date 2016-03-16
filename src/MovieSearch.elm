1fed61c61a66a2cafa214e7ecdfa1194482322a7
Husain Kassamnath <hkassamn@ucsc.edu>
2016-03-15 18:03:15 -0700

    Update MovieSearch.elm

--- src/MovieSearch.elm
+++ src/MovieSearch.elm
@@ -59,7 +59,7 @@ view : Model -> Html
 view model =
   div [ class "box" ]
   [ h2 [] [ text "Please click on your favorite image or enter your movie search" ]
-   , img [ alt "Mountain View", src "http://www.puppyheaven.com/media/imagegallery/leonardo-dicaprio-celebrity-gallery-img-2015-12-24-21-28-18.jpg", attribute "style" "width:300px;height:300px;" ]
+   , img [ alt "Actor", src "http://www.puppyheaven.com/media/imagegallery/leonardo-dicaprio-celebrity-gallery-img-2015-12-24-21-28-18.jpg", attribute "style" "width:300px;height:300px;" ]
   [ ]
   
     , div [  ]
